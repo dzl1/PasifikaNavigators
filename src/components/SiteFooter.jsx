a9ffcd713cd3063d6bbe5997bf419ff4b6eba552
Dave <dave.lasike@live.com>
2026-07-27 14:35:42 +1200

    Remove footer contact number

--- src/components/SiteFooter.jsx
+++ src/components/SiteFooter.jsx
@@ -14,10 +14,6 @@ export default function SiteFooter({ id }) {
           <span>Email</span>
           <a href="mailto:office@pasifikanavigators.nz">office@pasifikanavigators.nz</a>
         </div>
-        <div>
-          <span>Phone</span>
-          <a href="tel:+64212178028">+64 21 2178028</a>
-        </div>
       </div>
       <p className="site-footer__copyright">© {new Date().getFullYear()} Pasifika Navigators. All rights reserved.</p>
     </footer>
